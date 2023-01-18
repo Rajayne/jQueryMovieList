64548a9f601af2ff8e9955139f09e365c64cf9c3
Rajayne <ryann.j.nealey@gmail.com>
2023-01-17 17:55:42 -0800

    Include Practice Directions

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-/* 1. Create form with two inputs and a submit button using jQuery
+/* 
+1. Create form with two inputs and a submit button using jQuery
+2. When the form is submitted, capture the values for each input
+ and append to the DOM along with a button to remove from the DOM.
+3. When the remove button is clicked, remove each title and rating.
+
 $('body').append('<form></form>')
 
 $('form').prepend("<input type='range' id='rating' min='0' max='10' list='markers'>")
